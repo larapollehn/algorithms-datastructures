82e5cdd9014bf78ecaa6f9e55785812ac443c109
LarryWho11 <lara.pollehn@googlemail.com>
2020-04-25 13:29:56 +0200

    another methos

--- src/datastructure/BinarySearchTree.ts
+++ src/datastructure/BinarySearchTree.ts
@@ -17,58 +17,83 @@ export class BinarySearchTree<T> {
     public root: TreeNode<T>;
 
     insert(t: T): void {
-    	let inserted_node: TreeNode<T> = new TreeNode(t);
-		if (!this.root){
-			this.root = inserted_node;
-		}else{
-			let current: TreeNode<T> = this.root;
-			let previous: TreeNode<T>;
+        let inserted_node: TreeNode<T> = new TreeNode(t);
+        if (!this.root) {
+            this.root = inserted_node;
+        } else {
+            let current: TreeNode<T> = this.root;
+            let previous: TreeNode<T>;
 
-			while (current !== null){
-				if (inserted_node.data < current.data){
-					previous = current;
-					current = current.left;
-				} else {
-					previous = current;
-					current = current.right;
-				}
-			}
+            while (current !== null) {
+                if (inserted_node.data < current.data) {
+                    previous = current;
+                    current = current.left;
+                } else {
+                    previous = current;
+                    current = current.right;
+                }
+            }
 
-			if (inserted_node.data < previous.data){
-				previous.left = inserted_node;
-			} else{
-				previous.right = inserted_node;
-			}
-		}
+            if (inserted_node.data < previous.data) {
+                previous.left = inserted_node;
+            } else {
+                previous.right = inserted_node;
+            }
+        }
     }
 
-	contains(t: T): boolean {
-    	return false;
-	}
+    contains(t: T): boolean {
+        if (!this.root) {
+            return false;
+        } else {
+            let current = this.root;
+            while (current !== null) {
+                if (current.data === t){
+                    return true;
+                }else if (current.data > t){
+                    current = current.left;
+                }else{
+                    current = current.right;
+                }
+            }
+            return false;
+        }
+    }
 
-	/*
-	 * Tips: Use high order function
-	 */
-	preOder(): Array<T> {
-    	return new Array<T>();
-	}
+/*
+ * Tips: Use high order function
+ */
+preOder()
+:
+Array < T > {
+    return new Array<T>();
+}
 
-	/*
-	 * Tips: Use high order function
-	 */
-	inOrder(): Array<T> {
-    	return new Array<T>();
-	}
+/*
+ * Tips: Use high order function
+ */
+inOrder()
+:
+Array < T > {
+    return new Array<T>();
+}
 
-	/*
-	 * Tips: Use high order function
-	 */
-	levelOrder(): Array<T> {
-    	return new Array<T>();
-	}
+/*
+ * Tips: Use high order function
+ */
+levelOrder()
+:
+Array < T > {
+    return new Array<T>();
+}
 
-	equals(otherTree: BinarySearchTree<T>): boolean{
-		return true;
-	}
+equals(otherTree
+:
+BinarySearchTree<T>
+):
+boolean
+{
+    return true;
+}
 
 }
--- tests/datastructure/BinarySearchTree.spec.ts
+++ tests/datastructure/BinarySearchTree.spec.ts
@@ -1,4 +1,5 @@
 import {BinarySearchTree, TreeNode} from "../../src/datastructure/BinarySearchTree";
+import exp = require("constants");
 
 test('creating a node works', () => {
     let node: TreeNode<number> = new TreeNode(4);
@@ -42,4 +43,29 @@ test('inserting a node into the BinarySearchTree works', () => {
     expect(tree.root.right.data).toBe(35);
     expect(tree.root.right.left.data).toBe(31);
     expect(tree.root.right.right).toBe(null);
+});
+
+test('checking if tree conatins node works', () => {
+    const tree: BinarySearchTree<number> = new BinarySearchTree();
+    tree.insert(27);
+    tree.insert(14);
+    tree.insert(35);
+    tree.insert(19);
+    tree.insert(31);
+    tree.insert(42);
+    tree.insert(10);
+    expect(tree.contains(27)).toEqual(true);
+    expect(tree.contains(14)).toEqual(true);
+    expect(tree.contains(35)).toEqual(true);
+    expect(tree.contains(19)).toEqual(true);
+    expect(tree.contains(31)).toEqual(true);
+    expect(tree.contains(42)).toEqual(true);
+    expect(tree.contains(10)).toEqual(true);
+
+    expect(tree.contains(0)).toEqual(false);
+    expect(tree.contains(17)).toEqual(false);
+    expect(tree.contains(9)).toEqual(false);
+    expect(tree.contains(239)).toEqual(false);
+    expect(tree.contains(-6)).toEqual(false);
+    expect(tree.contains(27.5)).toEqual(false);
 });
