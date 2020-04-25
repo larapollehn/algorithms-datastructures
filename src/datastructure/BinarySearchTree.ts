0590e85a75be6f5f9f54f578cf18ec9f80d6fc71
Long Nguyen <long.nguyen2306@protonmail.com>
2020-04-25 11:26:57 +0200

    API for binary search tree

---
+++ src/datastructure/BinarySearchTree.ts
@@ -0,0 +1,54 @@
+export class TreeNode<T> {
+    public data: T;
+    public left: TreeNode<T>;
+    public right: TreeNode<T>;
+
+
+    constructor(data: T, left: TreeNode<T>, right: TreeNode<T>) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+export class BinarySearchTree<T> {
+    public head: TreeNode<T>;
+
+    insert(t: T): void {
+
+    }
+
+    delete(t: T): void {
+
+	}
+
+	contains(t: T): boolean {
+    	return false;
+	}
+
+	/*
+	 * Tips: Use high order function
+	 */
+	preOder(): Array<T> {
+    	return new Array<T>();
+	}
+
+	/*
+	 * Tips: Use high order function
+	 */
+	inOrder(): Array<T> {
+    	return new Array<T>();
+	}
+
+	/*
+	 * Tips: Use high order function
+	 */
+	levelOrder(): Array<T> {
+    	return new Array<T>();
+	}
+
+	equals(otherTree: BinarySearchTree<T>): boolean{
+		return true;
+	}
+
+}
